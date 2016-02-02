b2aa8585a99af2df096e3258200de578256645db
Aubrey Wullschleger <aubrey.wullschleger@gmail.com>
2016-02-02 17:35:42 -0600

    connected to simple server

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,11 +1,21 @@
 var React = require('react');
 var ListItem = require('./ListItem.jsx');
+var HTTP = require('../services/httpService.js');
 
-var ingredients = [{"id": 1, "text": "ham"}, {"id": 2, "text": "cheese"}, {"id": 3, "text": "potato"}];
+// var ingredients = [{"id": 1, "text": "pineapple"}, {"id": 2, "text": "bacon"}, {"id": 3, "text": "chili"}];
 
 var List = React.createClass({
+  getInitialState: function() {
+    return {ingredients: []};
+  },
+  componentWillMount: function() {
+    HTTP.get('/ingredients')
+    .then(function(data) {
+      this.setState({ingredients: data});
+    }.bind(this));
+  },
   render: function() {
-    var listItems = ingredients.map(function(item) {
+    var listItems = this.state.ingredients.map(function(item) {
       return <ListItem key={item.id} ingredient={item.text}/>;
     });
     return (
